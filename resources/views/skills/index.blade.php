d3dc5f51e39da0869d7ab2951693a22d713537c2
Nino-YM <119847747+Nino-YM@users.noreply.github.com>
2024-06-06 17:01:29 +0200

    changement du style sur la page universe

--- resources/views/skills/index.blade.php
+++ resources/views/skills/index.blade.php
@@ -6,9 +6,9 @@
     <div class="row">
         @foreach($skills as $skill)
             <div class="col-md-3 text-center">
-                <a href="{{ route('skills.show', $skill) }}">
-                    <img src="{{ asset('storage/' . strtolower($skill->name) . '.png') }}" alt="{{ $skill->name }} icon" style="width: auto; height: 50px; object-fit: contain; vertical-align: middle;">
-                    <h3>{{ $skill->name }}</h3>
+                <a href="{{ route('skills.show', $skill) }}" class="skill-link">
+                    <img src="{{ asset('storage/' . strtolower($skill->name) . '.png') }}" alt="{{ $skill->name }} icon" class="skill-icon">
+                    <span class="skill-name">{{ $skill->name }}</span>
                 </a>
             </div>
         @endforeach
